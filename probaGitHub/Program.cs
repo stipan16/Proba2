f570f5fa4b5547cbbd37f2abd44cbb685b5c546e
stipan16 <slausi00@fesb.hr>
2015-03-09 15:19:43 +0100

    novi program

---
+++ probaGitHub/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace probaGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("proba2");
+            Console.ReadLine();
+        }
+    }
+}
